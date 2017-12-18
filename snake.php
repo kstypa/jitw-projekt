20b93ceeb78edb8036055b7d7aa9d54214d32f26
Karol Stypa <bury09@gmail.com>
2017-12-18 02:40:09 +0100

    snake game code added, scores WIP

--- snake.php
+++ snake.php
@@ -13,6 +13,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="./style.css">
+		<script src="./js/snake.js" type="text/javascript"></script>
 	</head>
 
 	<body>
@@ -26,6 +27,10 @@
 				if ($_SESSION['loggedin'])
 				{
 					echo '<h2>Możesz grać!</h2>';
+					echo '
+						<div class="snake-canvas">
+							<canvas id="stage" height="400" width="520"></canvas>
+						</div>';
 				}
 			}
 
