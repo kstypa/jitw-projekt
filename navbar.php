d8710c694ff0ea9bd3d4c5ab62e8d11adb29cfcb
Karol Stypa <bury09@gmail.com>
2018-01-15 01:43:10 +0100

    changed navbar profile tab to username

--- navbar.php
+++ navbar.php
@@ -34,7 +34,7 @@ if(isset($_SESSION['loggedin'])) {
                     echo '
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Profil
+                            '.$login.'
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item" href="./profile.php?id='.$uid.'">Zobacz profil</a>
